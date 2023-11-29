6fd34514ffd5a3797f326f248ec4190582f24c42
Adeshola Ayomide <josephay125d@gmail.com>
2023-11-29 02:15:46 -0800

    display room

--- src/components/DisplayRooms.jsx
+++ src/components/DisplayRooms.jsx
@@ -20,52 +20,64 @@ const DisplayRooms = () => {
   );
   console.log(getImg);
 
-  // const Rooms= [
-  //   {
-  //     image:"https://img.freepik.com/free-photo/modern-studio-apartment-design-with-bedroom-living-space_1262-12375.jpg?w=740&t=st=1701021559~exp=1701022159~hmac=d0d12f6e11d6298ef245c98067fea82b33379873733449ce8e5cde2ab53a1243"
-  //   },
-  //   {
-  //     image:"https://img.freepik.com/free-photo/modern-studio-apartment-design-with-bedroom-living-space_1262-12375.jpg?w=740&t=st=1701021559~exp=1701022159~hmac=d0d12f6e11d6298ef245c98067fea82b33379873733449ce8e5cde2ab53a1243https://img.freepik.com/premium-photo/hotel_664434-4822.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais"
-  //   },
-  //   {
-  //     image:"https://img.freepik.com/free-photo/luxury-bedroom-suite-resort-high-rise-hotel-with-working-table_105762-1783.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais"
-  //   },
-  //   {
-  //     image:"https://img.freepik.com/free-photo/3d-rendering-modern-luxury-bedroom-suite-bathroom_105762-1936.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais"
-  //   },
-  //   {
-  //     image:"https://img.freepik.com/premium-photo/modern-bedroom-with-working-desk-bedding_41487-836.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais"
-  //   },
-  //   {
-  //     image:"https://img.freepik.com/premium-photo/luxury-roombedroom-interior_940802-664.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais"
-  //   },
-  //   {
-  //     image:"https://img.freepik.com/free-photo/bedroom-with-bed-chair-front-sliding-glass-door_1340-25294.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais"
-  //   },
-  //   {
-  //     image:"https://img.freepik.com/free-photo/modern-luxury-bedroom-suite-bathroom_105762-1791.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais"
-  //   },
-  //   {
-  //     image:"https://img.freepik.com/free-photo/luxury-bedroom-interior-with-rich-furniture-scenic-view-from-walkout-deck_1258-111480.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais"
-  //   },
-  //   {
-  //     image:"https://img.freepik.com/premium-photo/travel-concept-beautiful-modern-mansions-hotels-bedroom-interior-design_872147-40588.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais"
-  //   },
-  //   {
-  //     image:"https://img.freepik.com/premium-photo/luxury-modern-cozy-sea-view-double-bedroom_36036-382.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais"
-  //   },
-  //   {
-  //     image:"https://img.freepik.com/free-photo/3d-rendering-beautiful-comtemporary-luxury-bedroom-suite-hotel-with-tv_105762-2071.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais"
-  //   },
-  // ]
+  const Rooms = [
+    {
+      image:
+        "https://img.freepik.com/free-photo/modern-studio-apartment-design-with-bedroom-living-space_1262-12375.jpg?w=740&t=st=1701021559~exp=1701022159~hmac=d0d12f6e11d6298ef245c98067fea82b33379873733449ce8e5cde2ab53a1243",
+    },
+    {
+      image:
+        "https://img.freepik.com/free-photo/modern-studio-apartment-design-with-bedroom-living-space_1262-12375.jpg?w=740&t=st=1701021559~exp=1701022159~hmac=d0d12f6e11d6298ef245c98067fea82b33379873733449ce8e5cde2ab53a1243https://img.freepik.com/premium-photo/hotel_664434-4822.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais",
+    },
+    {
+      image:
+        "https://img.freepik.com/free-photo/luxury-bedroom-suite-resort-high-rise-hotel-with-working-table_105762-1783.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais",
+    },
+    {
+      image:
+        "https://img.freepik.com/free-photo/3d-rendering-modern-luxury-bedroom-suite-bathroom_105762-1936.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais",
+    },
+    {
+      image:
+        "https://img.freepik.com/premium-photo/modern-bedroom-with-working-desk-bedding_41487-836.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais",
+    },
+    {
+      image:
+        "https://img.freepik.com/premium-photo/luxury-roombedroom-interior_940802-664.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais",
+    },
+    {
+      image:
+        "https://img.freepik.com/free-photo/bedroom-with-bed-chair-front-sliding-glass-door_1340-25294.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais",
+    },
+    {
+      image:
+        "https://img.freepik.com/free-photo/modern-luxury-bedroom-suite-bathroom_105762-1791.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais",
+    },
+    {
+      image:
+        "https://img.freepik.com/free-photo/luxury-bedroom-interior-with-rich-furniture-scenic-view-from-walkout-deck_1258-111480.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais",
+    },
+    {
+      image:
+        "https://img.freepik.com/premium-photo/travel-concept-beautiful-modern-mansions-hotels-bedroom-interior-design_872147-40588.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais",
+    },
+    {
+      image:
+        "https://img.freepik.com/premium-photo/luxury-modern-cozy-sea-view-double-bedroom_36036-382.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais",
+    },
+    {
+      image:
+        "https://img.freepik.com/free-photo/3d-rendering-beautiful-comtemporary-luxury-bedroom-suite-hotel-with-tv_105762-2071.jpg?size=626&ext=jpg&ga=GA1.1.26054885.1689417218&semt=ais",
+    },
+  ];
   return (
     <div>
-      <div>
-        <div className="container ">
-          <div className="row justify-content-between ">
-            <div className="shadow get_bg pb-3 col-md-4 col-12 rounded">
+      <div className="container ">
+        <div className="row  ">
+          <div className="d-md-flex gap-md-3 justify-content-between">
+            <div className="shadow  get_bg pb-3 col-md-4 col-12 rounded">
               <div className="text-light">
-                <h4 className="px-4 my-">Search</h4>
+                <h4 className="px-md-4 px-2">Search</h4>
                 <div className="px-4">
                   <label htmlFor="" className="fw-bold">
                     Destination
@@ -143,14 +155,17 @@ const DisplayRooms = () => {
               </div>
             </div>
 
-            <div className="room-h border shadow d-md-flex justify-content-between  col-md-8 gap-md-3 py-3 px-4">
-            <div className="col-md-4  col-12">
-                <img src={getImg.image} className="img-fluid col-md-4" />
+            <div className="mx-auto col-md-8 ">
+              <div className="border d-md-flex  shadow justify-content-between gap-md-3 py-3 px-4  col-12">
+                <div className="col-md-4 col-12">
+                  <img
+                    src={getImg.image}
+                    className="rounded room-img col-md-12 col-12"
+                  />
                 </div>
                 <div className="col-md-8 col-12 px-1">
-                  
-                  <div className="d-md-flex mt-md-3 d-flex">
-                    <h5 style={{color:'#a99808'}} className="w-100 fw-bold">
+                  <div className="d-md-flex mt-md-3 mt-3 d-flex">
+                    <h5 style={{ color: "#a99808" }} className="w-100 fw-bold">
                       Tower Street Apartments
                     </h5>
                     <div className="fw-bold">Excellent</div>
@@ -167,31 +182,34 @@ const DisplayRooms = () => {
                     <div className="fw-bold fs-4">{getImg.price}</div>
                   </div>
                   <div className="d-flex justify-content-between">
-                  <div className="text-success fw-bold">Free cancellation</div>
-                  <div>Include taxi and fee</div>
+                    <div className="text-success fw-bold">
+                      Free cancellation
+                    </div>
+                    <div>Include taxi and fee</div>
                   </div>
                   <div className="d-flex justify-content-between">
-                    <p className="text-success">you can cancel later, os lock in this great price today!</p>
-                    <button className="btn rounded col-4 py-1">SEE AVAILABILITY</button>
+                    <p className="text-success">
+                      you can cancel later, os lock in this great price today!
+                    </p>
+                    <button className="btn rounded col-4 py-0">
+                      SEE AVAILABILITY
+                    </button>
                   </div>
                 </div>
-                
-         
+              </div>
+              <div className="col-md-12 col-12 get-grid mt-3">
+                {Rooms.map((allimag) => (
+                  <>
+                    <div
+                      className="col-md-12 gap-style col-12"
+                      key={allimag.image}
+                    >
+                      <img src={allimag.image} className="img-fluid col-12  " />
+                    </div>
+                  </>
+                ))}
+              </div>
             </div>
-
-            {/* {
-              Rooms.map((allimag)=>(
-                <>
-                <div className='col-md-12 gap-style col-12 ' key={allimag.image}>
-                  <img  src={allimag.image} className='img-fluid col-12  ' />
-                </div>
-                </>
-              ))
-            } */}
-                     {/* <p>{getImg.name}</p>
-          <p>{getImg.star}</p>
-          <p>{getImg.day}</p>
-          <p>{getImg.location}</p> */}
           </div>
         </div>
       </div>
