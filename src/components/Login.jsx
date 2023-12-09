87bd769b468730fdd96270f8c6d1aefd61c8adae
Adeshola Ayomide <josephay125d@gmail.com>
2023-12-09 09:34:18 -0800

    code update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,8 +13,24 @@ const Login = () => {
         <div className="row">
           <div
             style={{ backgroundColor: "#a99808 " }}
-            className="top-div text-center py-5"
+            className="top-div text-center py-4"
           >
+                 <Link to={"/create"}>
+            <button class="button">
+              <div class="button-box">
+                <span class="button-elem">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 46 40">
+                    <path d="M46 20.038c0-.7-.3-1.5-.8-2.1l-16-17c-1.1-1-3.2-1.4-4.4-.3-1.2 1.1-1.2 3.3 0 4.4l11.3 11.9H3c-1.7 0-3 1.3-3 3s1.3 3 3 3h33.1l-11.3 11.9c-1 1-1.2 3.3 0 4.4 1.2 1.1 3.3.8 4.4-.3l16-17c.5-.5.8-1.1.8-1.9z"></path>
+                  </svg>
+                </span>
+                <span class="button-elem">
+                  <svg viewBox="0 0 46 40">
+                    <path d="M46 20.038c0-.7-.3-1.5-.8-2.1l-16-17c-1.1-1-3.2-1.4-4.4-.3-1.2 1.1-1.2 3.3 0 4.4l11.3 11.9H3c-1.7 0-3 1.3-3 3s1.3 3 3 3h33.1l-11.3 11.9c-1 1-1.2 3.3 0 4.4 1.2 1.1 3.3.8 4.4-.3l16-17c.5-.5.8-1.1.8-1.9z"></path>
+                  </svg>
+                </span>
+              </div>
+            </button>
+                  </Link>
             <div className="d-flex justify-content-center mt-5 gap-4">
               <Link to={"/create"} className="text-light text-decoration-none">
                 Sign Up
