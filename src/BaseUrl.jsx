1416c0d0ba7cc462846af9b5bc8913a1b3a591b6
Adeshola Ayomide <josephay125d@gmail.com>
2023-12-14 02:22:12 -0800

    update code

--- src/BaseUrl.jsx
+++ src/BaseUrl.jsx
@@ -1,9 +1,11 @@
 let baseUrl;
 
 if (process.env.NODE_ENV === 'production') {
-    baseUrl = 'https://book-your-stay.onrender.com/user';
+    baseUrl = 'https://hotel-management-kj53.onrender.com/user';
+
 } else {
     baseUrl = 'http://localhost:5600/user/';
 }
+
 
 export default baseUrl;
