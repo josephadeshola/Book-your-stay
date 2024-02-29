1d38aae0c54891d76fc537d495c972ee8c940ce7
Adeshola Ayomide <josephay125d@gmail.com>
2024-02-29 07:21:10 -0800

    code update

--- src/components/FoodNav.jsx
+++ src/components/FoodNav.jsx
@@ -1,71 +1,85 @@
-import React from 'react'
-import image from '../assets/image/download.png'
-import { Link } from 'react-router-dom'
+import React from "react";
+import image from "../assets/image/download.png";
+import { Link } from "react-router-dom";
 const FoodNav = () => {
-    return (
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg fixed-top mb-5 nav-style getHeight">
+        <div class="container-fluid">
+          <a class="navbar-brand fw-bold d-flex" href="#">
+            <img className="img-H" src="https://img.freepik.com/premium-vector/logo-rastaraunt_985938-117.jpg?w=740" alt="" />
         <div>
-            <header id="header" class="fixed-top d-flex align-items-cente">
-    <div class="container-fluid container-xl d-flex align-items-center justify-content-lg-between">
+          <span className="span1">D</span>
+          <span className="span2">R</span>
+          <span className="span3">C</span>
+        </div>
+          </a>
+          <div className="d-none  d-md-block col-10 mx-auto">
+            <div className="d-flex justify-content-between">
 
-      <h1 class="logo me-auto me-lg-0"><a href="index.html">DRc</a></h1>
-      
-       <a href="index.html" class="logo me-auto me-lg-0"></a>
+            <form class="d-flex justify-content-between mt-1 col-6 py-2 mx-auto" role="search">
+              <input
+                className="form-control  py-3 me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                />
+              <button class="btn px-5 btn-outline-light" type="submit">
+                Search
+              </button>
+            </form>
 
-      <nav id="navbar" class="foodnavbar order-last order-lg-0">
-        <ul>
-          <li><Link class="nav-link scrollto active" to={"/"}>Home</Link></li>
-          <li><a class="nav-link scrollto" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#menu">Menu</a></li>
-          <li><a class="nav-link scrollto" href="#specials">Specials</a></li>
-          <li><a class="nav-link scrollto" href="#events">Events</a></li>
-          <li><a class="nav-link scrollto" href="#chefs">Chefs</a></li>
-          <li><a class="nav-link scrollto" href="#gallery">Gallery</a></li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
+            <button class="btn px-5  btn-h mt-3 btn-outline-light">GET STARTED</button>
+                </div>
+          <hr className="hr-style" />
+          </div>
+          <button
+            class="navbar-toggler border border-none"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i class="fa-solid fa-bars py-1  fs-3 px-1 text-light"></i>
+            {/* <span class="navbar-toggler-icon"></span> */}
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <div className="d-md-none d-block">
+
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
               </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Link
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+              </li>
             </ul>
-          </li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav>
-      <a href="#book-a-table" class="book-a-table-btn scrollto d-none d-lg-flex">Book a table</a>
-
-    </div>
-  </header>
-  <section id="hero" class="d-flex align-items-center">
-    <div class="container position-relative text-center text-lg-start" data-aos="zoom-in" data-aos-delay="100">
-      <div class="row">
-        <div class="col-lg-8">
-          <h1>Welcome to <span>Restaurantly</span></h1>
-          <h2>Delivering great food for more than 18 years!</h2>
-
-          <div class="btns">
-            <a href="#menu" class="btn-menu animated fadeInUp scrollto">Our Menu</a>
-            <a href="#book-a-table" class="btn-book animated fadeInUp scrollto">Book a Table</a>
+            </div>
+            <form class="d-flex d-md-none d-block" role="search">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button class="btn px-4 text-light btn-outline-ligh" type="submit">
+                Search
+              </button>
+            </form>
           </div>
         </div>
-        <div class="col-lg-4 d-flex align-items-center justify-content-center position-relative" data-aos="zoom-in" data-aos-delay="200">
-          {/* <a href="https://www.youtube.com/watch?v=u6BOC7CDUTQ" class="glightbox play-btn"></a> */}
-        </div>
-
-      </div>
+      </nav>
     </div>
-  </section>
-        </div>
-    )
-}
+  );
+};
 
-export default FoodNav
+export default FoodNav;
--- src/components/FoodOrd.jsx
+++ src/components/FoodOrd.jsx
@@ -1,22 +1,179 @@
-import React from 'react'
-import FoodNav from './FoodNav'
-import styles from '../components/styles/FoodOrd.css'
-import axios from 'axios'
-import baseUrl from '../BaseUrl'
+import React, { useEffect } from "react";
+import FoodNav from "./FoodNav";
+import styles from "../components/styles/FoodOrd.css";
+import axios from "axios";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const FoodOrd = () => {
-    axios.post(baseUrl + "/product")
-    
+  const [product, setProduct] = useState([]);
+  let endPoint = "http://localhost:5600/api/products";
+  //   let endPoint = "https://forkify-api.herokuapp.com/api/v2/recipes"
+  useEffect(() => {
+    axios
+      .get(endPoint)
+      .then((res) => {
+        console.log(res, "product");
+        setProduct(res.data);
+      })
+      .catch((err) => {
+        console.log(err, "product error");
+      });
+  }, []);
+
   return (
     <div>
-       <FoodNav/>
-       <main id="main">
-        <select name="" id="about">
-            
-        </select>
-       </main>
+      <FoodNav />
+      <main id="main">
+        <section>
+          <div className="container">
+            <div className="d-md-flex getMarginTop  justify-content-between gap-md-3">
+              <div className="d-none px-4 d-md-block shadow fixed-div">
+               
+              <ul class="py-4 ">
+                <h5 className="py-3">Sort by</h5>
+              <li style={{ listStyle: "none" }} class="nav-item py-1 d-flex gap-3">
+                <i class="fa-brands fa-nfc-symbol fs-3"></i>
+                <Link class="nav-link" to={"/"}>
+                Near me
+                </Link>
+              </li>
+                <hr/>
+              <li style={{ listStyle: "none" }} class="nav-item py-1 d-flex gap-3">
+              <i class="fa-regular fa-thumbs-up fs-3"></i>
+                <Link class="nav-link " to={"/about"}>
+                 Best rated
+                </Link>
+              </li>
+                <hr />
+              <li style={{ listStyle: "none" }} class="nav-item py-1 d-flex gap-3">
+              <i class="fa-solid fa-car-burst fs-3"></i>
+                <Link class="nav-link "to={""}>
+                  Delivery free
+                </Link>
+              </li>
+                <hr />
+            <h5 className="py-3">Popular filters</h5>
+              <li style={{ listStyle: "none" }} class="nav-item py-1 d-flex gap-3 ">
+              <i class="fa-solid fa-drumstick-bite fs-3"></i>
+                <Link class="nav-link" to={"/"}>
+                 Chicken
+                </Link>
+              </li>
+                <hr className="mx-auto" />
+              <li style={{ listStyle: "none" }} class="nav-item py-1 d-flex gap-3">
+              <i class="fa-solid fa-bowl-rice fs-3"></i>
+                <Link class="nav-link " to={"/about"}>            
+                    Jollof
+                </Link>
+              </li>
+                <hr />
+              <li style={{ listStyle: "none" }} class="nav-item py-1 d-flex gap-3 ">
+              <i class="fa-solid fa-bowl-rice fs-3"></i>
+                <Link class="nav-link "to={""}>
+                  Nigerian
+                </Link>
+              </li>
+                <hr />
+            </ul>
+              </div>
+              <div className="scroll-div">
+                <h1>Restaurants delivery in Ogbomoso</h1>
+                <h3>
+                  <i class="fs-3 fa-brands fa-accessible-icon"></i> Promotions
+                </h3>
+                <div className="d-flex getOverflow py-2  gap-3">
+                    {product.map((eachProduct) => (
+                 <>
+                  <div className="col-md-4 col-12 px-md-3 px-4 shadow rounded">
+                        <div className="img-div  py-2 col-div">
+                          <img
+                            className="col-12"
+                            src={eachProduct.image}
+                            alt=""
+                          />
+                        </div>
+                        <div>
+                          <div className="d-flex justify-content-between">
+                            <h4 className="h4-tag"> Burger King</h4>
+                            <p
+                              style={{ color: "rgb(207, 207, 209)" }}
+                              className="fw-bold"
+                            >
+                           
+                              Burger King
+                            </p>
+                          </div>
+                          <h5 style={{ color: " #AE8625" }}>
+                            <i class="fa-solid fs-3 fa-person-running"></i>FREE
+                          </h5>
+                        </div>
+                  </div>
+                      </>
+                    ))}
+                </div>
+                <div className="d-flex getOverflow py-2  gap-3">
+                    {product.map((eachProduct) => (
+                        <>
+                  <div className="col-md-4 col-12 px-md-3 px-4 shadow rounded">
+                        <div className="img-div  py-2 col-div">
+                          <img
+                            className="col-12"
+                            src={eachProduct.image}
+                            alt=""
+                          />
+                        </div>
+                        <div>
+                          <div className="d-flex justify-content-between">
+                            <h4 className="h4-tag"> Burger King</h4>
+                            <p
+                              style={{ color: "rgb(207, 207, 209)" }}
+                              className="fw-bold"
+                            >
+                           
+                              Burger King
+                            </p>
+                          </div>
+                          <h5 style={{ color: " #AE8625" }}>
+                            <i class="fa-solid fs-3 fa-person-running"></i>FREE
+                          </h5>
+                        </div>
+                  </div>
+                      </>
+                    ))}
+                </div>
+                cupiditate, magni voluptatum dolorum quae esse consequuntur
+                pariatur a ipsa fugiat? Nostrum voluptatem id, fuga quae sit
+                doloremque ab numquam minima mollitia maxime deserunt, iure a
+                vel reprehenderit at doloribus exercitationem quo harum delectus
+                veritatis totam! Voluptas, impedit sequi. Mollitia eligendi
+                exercitationem eius dicta quisquam, quaerat atque odit rerum,
+                nisi, vel suscipit? Aperiam fugit sed impedit error aliquid.
+                Minus magni voluptates, voluptas nemo sed illo consectetur,
+                maxime voluptatem odit adipisci fuga quasi dignissimos
+                aspernatur, ratione temporibus accusamus sunt id rem labore
+                provident dolor? Odit, atque ullam. Corrupti aspernatur omnis
+                ipsum ratione nemo quo commodi odio alias similique autem ut
+                nostrum libero tenetur natus eaque sit consequuntur neque
+                sapiente in, amet reprehenderit necessitatibus nesciunt at
+                eligendi. Cum itaque optio quae nam unde quasi atque earum nobis
+                quibusdam iste accusantium vel praesentium error repudiandae
+                vitae facere dicta eos tempore, recusandae rem provident
+                suscipit! Autem asperiores fuga et ipsam, deleniti repellendus
+                cum commodi adipisci, corrupti amet quia ratione. Magnam debitis
+                doloribus, eos voluptatibus corporis expedita accusamus sit
+                dolore possimus neque veritatis modi illum nostrum et ea autem
+                officia aperiam alias dolorem consequuntur unde! Fugiat natus a
+                deserunt excepturi culpa aut fuga doloremque obcaecati
+                cupiditate ex magnam aliquid consectetur impedit, sunt quis
+                ducimus unde facere voluptatum. Cum.
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default FoodOrd
+export default FoodOrd;
